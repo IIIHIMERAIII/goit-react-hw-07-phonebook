1caf91cfbd451c426990ff1fe1758579700fd2f3
Maksym <PTNPNH722@gmail.com>
2023-01-06 12:41:44 +0200

    trash fix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from "react-redux";
 import { store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
